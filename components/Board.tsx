72ff102b34d8c846b9ddc4fd3cad0e636f85b597
Mrinal Mallik <mrinal@nagorik.tech>
2023-06-13 17:01:18 +0600

    appwrite react beautifull dnd and zustand for state management installed

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+"use client";
+import React, { useEffect } from "react";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 function Board() {
+  useEffect(() => {
+    //getBoard()
+  });
+    21
   return (
-    <div>Board</div>
-  )
+    <DragDropContext>
+      <Droppable droppableId="board" direction="horizontal" type="column">
+        {(provided) => <div>{}</div>}
+      </Droppable>
+    </DragDropContext>
+  );
 }
 
-export default Board
+export default Board;
